12a2b4e883e38030d064c638bcb41a1f5cd09e09
gopalnrai <gopalnrai@gmail.com>
2020-06-03 19:11:48 +0530

    Made changes for array translation.

--- jkind-common/src/jkind/smv/visitors/SMVTypeVisitor.java
+++ jkind-common/src/jkind/smv/visitors/SMVTypeVisitor.java
@@ -1,10 +1,11 @@
 package jkind.smv.visitors;
 
+import jkind.smv.SMVArrayType;
 import jkind.smv.SMVNamedType;
 import jkind.smv.SMVSubrangeIntType;
 
 public interface SMVTypeVisitor<T> {
-	//public T visit(ArrayType e);
+	public T visit(SMVArrayType e);
 	//public T visit(EnumType e);
 	public T visit(SMVNamedType e);
 	//public T visit(RecordType e);
