4ad96dc7db96ffb5b82bc90090d760335c7bfe45
gopalnrai <gopalnrai@gmail.com>
2020-06-03 19:17:17 +0530

    Made changes for SMVFunction.

--- jkind-common/src/jkind/smv/visitors/SMVAstVisitor.java
+++ jkind-common/src/jkind/smv/visitors/SMVAstVisitor.java
@@ -1,12 +1,14 @@
 package jkind.smv.visitors;
 
 import jkind.smv.SMVEquation;
+import jkind.smv.SMVFunction;
 import jkind.smv.SMVModule;
 import jkind.smv.SMVProgram;
 import jkind.smv.SMVVarDecl;
 
 public interface SMVAstVisitor<T, S extends T> extends SMVExprVisitor<S>{
 	public T visit(SMVEquation smvEquation);
+	public T visit(SMVFunction smvFunction);
 	public T visit(SMVModule smvModule);
 	public T visit(SMVProgram smvProgram);
 	public T visit(SMVVarDecl smvVarDecl);
