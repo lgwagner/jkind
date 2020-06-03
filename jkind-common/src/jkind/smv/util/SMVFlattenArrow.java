36d3f14aef759a7687fd8a11f370661d53b8f633
gopalnrai <gopalnrai@gmail.com>
2020-06-03 19:15:36 +0530

    Made changes to remove some bugs.

--- jkind-common/src/jkind/smv/util/SMVFlattenArrow.java
+++ jkind-common/src/jkind/smv/util/SMVFlattenArrow.java
@@ -1,7 +1,6 @@
-package jkind.translation;
+package jkind.smv.util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import jkind.lustre.BinaryExpr;
@@ -10,68 +9,85 @@ import jkind.lustre.Equation;
 import jkind.lustre.Expr;
 import jkind.lustre.IdExpr;
 import jkind.lustre.IfThenElseExpr;
-import jkind.lustre.IntExpr;
 import jkind.lustre.Node;
 import jkind.lustre.Program;
-import jkind.lustre.RealExpr;
 import jkind.lustre.UnaryExpr;
 import jkind.lustre.VarDecl;
 import jkind.lustre.visitors.TypeAwareAstMapVisitor;
+import jkind.translation.ContainsArrowOperator;
+
 /*
  * flattening arrow operator ("->")
  * */
-public class FlattenArrow extends TypeAwareAstMapVisitor{
-	
+//TODO some functions are same as in SMVFlattenPre, consider having only one copy 
+public class SMVFlattenArrow extends TypeAwareAstMapVisitor {
+
 	private List<VarDecl> newLocals = new ArrayList<>();
 	private List<Equation> newEquations = new ArrayList<>();
+
 	private int counter = 0;
-	
-	
-	public FlattenArrow(Program program) {
+
+	public SMVFlattenArrow(Program program) {
 		super.visit(program);
 	}
 
 	public Node node(Node node) {
 		for (Equation eqn : node.equations) {
 			if (ContainsArrowOperator.check(eqn.expr)) {
-				Expr p = proc(eqn.expr);
-				createEqn(eqn.lhs, getExpr(p));
+				createEqn(eqn.lhs, proc(eqn.expr));
 			} else
 				newEquations.add(eqn);
 		}
-		List<VarDecl> localsList = getLocals(node.locals);
-		return new Node(node.location, node.id, node.inputs, node.outputs, getLocals(node.locals), newEquations, node.properties,
-				node.assertions, node.realizabilityInputs, node.contract, node.ivc);
+		return new Node(node.location, node.id, node.inputs, node.outputs,
+				getLocals(node.inputs, node.outputs, node.locals), newEquations, node.properties, node.assertions,
+				node.realizabilityInputs, node.contract, node.ivc);
 	}
-	
-	private List<VarDecl> getLocals(List<VarDecl> locals){
+
+	private List<VarDecl> getLocals(List<VarDecl> inputs, List<VarDecl> outputs, List<VarDecl> locals) {
 		List<VarDecl> tempLocals = new ArrayList<>();
 		tempLocals.addAll(locals);
-		for(VarDecl newLocalElement : newLocals ) {
+		for (VarDecl newLocalVar : newLocals) {
 			boolean elementExist = false;
-			for( VarDecl varDecl : locals) {
-				if(newLocalElement.id.equals(varDecl.id)) {
+			for (VarDecl varDecl : inputs) {
+				if (newLocalVar.id.equals(varDecl.id)) {
 					elementExist = true;
+					break;
 				}
 			}
-			if(!elementExist) {
-				tempLocals.add(newLocalElement);
+			if (!elementExist) {
+				for (VarDecl varDecl : outputs) {
+					if (newLocalVar.id.equals(varDecl.id)) {
+						elementExist = true;
+						break;
+					}
+				}
+			}
+			if (!elementExist) {
+				for (VarDecl varDecl : locals) {
+					if (newLocalVar.id.equals(varDecl.id)) {
+						elementExist = true;
+						break;
+					}
+				}
+			}
+			if (!elementExist) {
+				tempLocals.add(newLocalVar);
 			}
 		}
 		return tempLocals;
 	}
-	
 
-	private Expr getExpr(Expr expr) {
-		for (Equation eqn : newEquations) {
-			if (eqn.lhs.get(0).id.contentEquals(expr.toString())) {
-				return eqn.expr;
-			}
-		}
-		return expr;
-	}
+	// TODO consider removing this
+//	private Expr getExpr(Expr expr) {
+//		for (Equation eqn : newEquations) {
+//			if (eqn.lhs.get(0).id.contentEquals(expr.toString())) {
+//				return eqn.expr;
+//			}
+//		}
+//		return expr;
+//	}
 
-	
+	// TODO why to create new IdExpr, can return the original one itself
 	public Expr proc(Expr e) {
 		if (e instanceof IdExpr) {
 			IdExpr id = (IdExpr) e;
@@ -79,7 +95,7 @@ public class FlattenArrow extends TypeAwareAstMapVisitor{
 		} else if (e instanceof UnaryExpr) {
 			UnaryExpr ue = (UnaryExpr) e;
 			return visitUnaryExpr(ue);
-		}else if(e instanceof BoolExpr) {
+		} else if (e instanceof BoolExpr) {
 			BoolExpr be = (BoolExpr) e;
 			return new BoolExpr(be.value);
 		} else if (e instanceof BinaryExpr) {
@@ -89,33 +105,26 @@ public class FlattenArrow extends TypeAwareAstMapVisitor{
 			IfThenElseExpr ite = (IfThenElseExpr) e;
 			return visitIfThenElseExpr(ite);
 		}
-
 		return e;
 	}
-	
+
 	private Expr visitUnaryExpr(UnaryExpr ue) {
 		if (ContainsArrowOperator.check(ue)) {
-			if (ue.expr instanceof IdExpr) {
-				return createEqn(ue);
-			}else {
-				return new UnaryExpr(ue.op, proc(ue.expr));
-			}
-
+			return new UnaryExpr(ue.op, proc(ue.expr));
 		} else
 			return createEqn(ue);
 	}
 
 	private Expr visitBinaryExpr(BinaryExpr be) {
 		if (ContainsArrowOperator.check(be)) {
-			if(isTrivialExpr(be.left) & isTrivialExpr(be.right)){
-			//if((be.left instanceof IdExpr | be.left instanceof BoolExpr | be.left instanceof IntExpr | be.left instanceof RealExpr) & (be.right instanceof IdExpr | be.right instanceof BoolExpr)) {
+			if ((!ContainsArrowOperator.check(be.left)) & (!ContainsArrowOperator.check(be.right))) {
 				return createEqn(be);
-			}else {
+			} else {
 				return visitBinaryExpr(new BinaryExpr(proc(be.left), be.op, proc(be.right)));
 			}
 
 		} else
-			return createEqn(be);
+			return be;
 	}
 
 	private Expr visitIfThenElseExpr(IfThenElseExpr ite) {
@@ -128,35 +137,20 @@ public class FlattenArrow extends TypeAwareAstMapVisitor{
 		} else
 			return createEqn(ite);
 	}
-	
-	private Boolean isTrivialExpr(Expr e) {
-		if(e instanceof IdExpr) {
-			return true;
-		}else if(e instanceof BoolExpr) {
-			return true;
-		}else if(e instanceof IntExpr) {
-			return true;
-		}else if(e instanceof RealExpr) {
-			return true;
-		}else {
-			return false;
-		}
-		
-	}
 
 	private IdExpr createEqn(Expr expr) {
 		List<IdExpr> lhs = new ArrayList<IdExpr>();
 		lhs.add(new IdExpr(getFreshId().toString()));
 		newEquations.add(new Equation(lhs, expr));
-		newLocals.add(new VarDecl(lhs.get(0).id,super.getType(expr)));
-		typeReconstructor.addVariable(new VarDecl(lhs.get(0).id,super.getType(expr)));
+		newLocals.add(new VarDecl(lhs.get(0).id, super.getType(expr)));
+		typeReconstructor.addVariable(new VarDecl(lhs.get(0).id, super.getType(expr)));
 		return (IdExpr) lhs.get(0);
 	}
 
 	private IdExpr createEqn(List<IdExpr> lhs, Expr expr) {
 		newEquations.add(new Equation(lhs, expr));
-		newLocals.add(new VarDecl(lhs.get(0).id,super.getType(expr)));
-		typeReconstructor.addVariable(new VarDecl(lhs.get(0).id,super.getType(expr)));
+		newLocals.add(new VarDecl(lhs.get(0).id, super.getType(expr)));
+		typeReconstructor.addVariable(new VarDecl(lhs.get(0).id, super.getType(expr)));
 		return (IdExpr) lhs.get(0);
 	}
 
